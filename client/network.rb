7a35ee480a4322c7530505c6ea364d07f60549f3
Milo Trujillo <milo.trujillo@zdtunited.com>
2015-08-28 12:45:05 -0700

    Implemented connection class, beginning of ability to suspend and restore network connections

--- client/network.rb
+++ client/network.rb
@@ -1,35 +1,62 @@
 #!/usr/bin/env ruby
 
+=begin
+	This file contains all of the code that's run whenever we 'dial in' to a
+	server. That means all the networking code and some inter-thread stuff.
+=end
+
 require 'socket'
 require 'thread'
 require_relative 'util'
 
 $screenLock = Mutex.new
-NetworkPrint = true
 
-def readFromServer(sock)
-	begin
-		while( char = sock.recv(1) )
-			break if char.empty?
-			# This is probably really inefficient, but since it's the client
-			# we don't particularly care.
-			$screenLock.synchronize {
-				if( NetworkPrint )
-					print char
-				end
-			}
-		end
-	rescue
+class Connection
+	def initialize(sock, lines, cols)
+		@sock = sock
+		@networkPrint = true
+		@sock.puts(cols.to_s)
+		@sock.puts(lines.to_s)
+		recv = Thread.new { readFromServer() }
+		send = Thread.new { writeToServer() }
+		send.join
+		recv.kill
 	end
-	puts "=== Server has closed connection ==="
-end
 
-def writeToServer(sock)
-	begin
-		while( line = gets )
-			sock.puts(line)
+	# Stub function, but will later allow us to resume a connection if it's
+	# been suspended to go run some local shell stuff.
+	def resume
+		return
+	end
+
+	def readFromServer()
+		begin
+			while( char = @sock.recv(1) )
+				break if char.empty?
+				# This is probably really inefficient, but since it's the client
+				# we don't particularly care.
+				$screenLock.synchronize {
+					if( @networkPrint )
+						print char
+					end
+				}
+			end
+		rescue
 		end
-	rescue
+		puts "=== Server has closed connection ==="
+	end
+
+	def writeToServer()
+		begin
+			while( line = gets )
+				if( line.chomp == "!quit" )
+					return
+				else
+					@sock.puts(line)
+				end
+			end
+		rescue
+		end
 	end
 end
 
@@ -45,11 +72,6 @@ def connect(serveraddr, portno)
 		return
 	end
 	puts "=== Connected to Server ==="
-	sock.puts(cols.to_s)
-	sock.puts(lines.to_s)
-	recv = Thread.new { readFromServer(sock) }
-	send = Thread.new { writeToServer(sock) }
-	recv.join
-	send.join
+	conn = Connection.new(sock, lines, cols)
 	STDOUT.sync = state
 end
