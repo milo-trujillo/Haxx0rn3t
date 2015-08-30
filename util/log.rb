deb44a4b914aff00f614ba287931f66ab64a5048
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2015-08-30 15:07:25 -0400

    Moved logging system to nuetral ground

--- util/log.rb
+++ util/log.rb
@@ -4,11 +4,14 @@
 	This file defines how messages should be logged. At present it's just a
 	wrapper around 'puts', but later when the central server daemonizes we'll
 	have to port this to use files or syslog.
+
+	Note: DebugMode *MUST* be declared here if the logging system is going
+	to be used by both central and node servers!
 =end
 
-require_relative 'config'
-
 module Log
+	$debugMode = true
+
 	# Define some constants for log messages
 	# All messages must come with a log level to be printed
 	Debug   = 1
@@ -19,7 +22,7 @@ module Log
 	def Log.log(level, msg)
 		case level
 			when Debug
-				if( Configuration::DebugMode == true )
+				if( $debugMode == true )
 					puts "Debug: " + msg
 				end
 			when Info
@@ -30,6 +33,15 @@ module Log
 				puts "ERROR: " + msg
 			else
 				Log.log(Warning, "Malformed log message has improper log level")
+		end
+	end
+
+	def Log.setDebugMode(bool)
+		# Sometimes ruby's typing system is a bit silly
+		if( bool == true )
+			$debugMode = true
+		elsif( bool == false )
+			$debugMode = false
 		end
 	end
 end	
